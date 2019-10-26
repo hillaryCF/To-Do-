5a064979a0e402d3e05b9a377dfa0b1a9933d4ad
Hillary Cordero <h.cordero@CETAV001.local>
2019-10-25 19:10:44 -0600

    to do

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Sidebar from './section/sidebar';
+import Form from './section/board';
+
+
+import './app.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+      <Form/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/modal.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './modal.css';
+
+const Modal = ({ modalClose, show,modalSubmit }) => {
+  const modalChange = show ? "modal display-block" : "modal display-none";
+  return (
+    <React.Fragment>
+    <div className={modalChange}>
+      <p className="modal__question">¿Do you sure save this task?</p>
+      <button onClick={modalClose} className="modal__btn">Add other task</button>
+      <button type="submit" onSubmit={modalSubmit} className="modal__btn modal__btn-left">Show Tasks</button>
+    </div>
+    
+    </React.Fragment>
+  );
+};
+
+export default Modal
